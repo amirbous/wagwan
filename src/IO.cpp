6ec96d4b4f2ecdf4acc819bdc622d1fc38107aac
tiagogiraldez <ge92lur@mytum.de>
2024-10-24 11:56:56 +0200

    JSON parser

--- src/IO.cpp
+++ src/IO.cpp
@@ -2,13 +2,20 @@
 #include <vector>
 #include <string>
 #include "../include/IO.hpp"
-
+#include "../include/simdjson.h"
 #include "../include/graph.hpp"
 
 
 // important should be change just made placehoders to reduce compilation warnings
 std::vector<Node> readGraph(std::string filename) {
     std::cout << "reading graph from file " << filename << std::endl; 
+
+    // parse JSON
+    simdjson::ondemand::parser parser;
+    auto json = padded_string::load(filename);
+    simdjson::ondemand::document doc = parser.iterate(json);
+
+
     std::vector<Node> nodes;
     return nodes;
 }
