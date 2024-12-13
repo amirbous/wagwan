d6d63e609646ba59df4d90bda50f0b7de378a927
Amir Bouslama <a.amirbouslama@gmail.com>
2024-12-13 09:31:46 +0100

    lambda expressions in c++ are slow, if capture capture scope captures big entities, 20x times faster intersect and work periterations

--- include/intersectGraph.hpp
+++ include/intersectGraph.hpp
@@ -7,6 +7,9 @@
 #include <ogdf/basic/GraphAttributes.h>
 
 
+bool isSharedNode(ogdf::node n1, ogdf::node n2, ogdf::node n3, ogdf::node n4);
+int orientation(const ogdf::GraphAttributes& GA, ogdf::node a, ogdf::node b, ogdf::node c);
+bool onSegment(const ogdf::GraphAttributes& GA, ogdf::node a, ogdf::node b, ogdf::node c);
 bool edgesIntersect(const ogdf::GraphAttributes& GA, ogdf::edge& e1, ogdf::edge& e2);
 
 
